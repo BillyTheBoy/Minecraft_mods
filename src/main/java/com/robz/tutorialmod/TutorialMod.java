5be33e5ad4b83f1406d432a317142043d2513082
Robin Desaubry <robin.desaubry5@etu.univ-lorraine.fr>
2026-01-08 21:23:06 +0100

    first commit

--- src/main/java/com/robz/tutorialmod/TutorialMod.java
+++ src/main/java/com/robz/tutorialmod/TutorialMod.java
@@ -24,6 +24,7 @@ public class TutorialMod
 
     public TutorialMod(FMLJavaModLoadingContext context)
     {
+
         IEventBus modEventBus = context.getModEventBus();
 
         // Register the commonSetup method for modloading
